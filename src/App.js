2bdea24fca1fbe2cc7de71e6b5c52ce7691d40fc
luizgasparetto <luhmgasparetto@gmail.com>
2023-02-09 18:16:27 -0300

    final

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useState } from 'react';
+import { BsCloudSun, BsCloudMoon, BsCloudRain, BsCloudLightningRain, BsCloudSnow } from 'react-icons/bs';
+import './App.css'
 
-  function App() {
-    const [data, setData] = useState({})
-    const [location, setLocation] = useState('')
-  
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=97d54f6f736bc4d9d7014516da54aa0c`
-  
-    const searchLocation = (event) => {
-      if (event.key === 'Enter') {
-        axios.get(url).then((response) => {
-          setData(response.data)
-          console.log(response.data)
-        })
-        setLocation('')
+function App() {
+  const [weatherData, setWeatherData] = useState({});
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(async position => {
+      const { latitude, longitude } = position.coords;
+      setLocation({ latitude, longitude });
+
+      const API_KEY = '97d54f6f736bc4d9d7014516da54aa0c';
+      const API_URL = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+
+      try {
+        const response = await axios.get(API_URL);
+        setWeatherData(response.data);
+      } catch (error) {
+        console.error(error);
       }
+    });
+  }, []);
+
+  let time = null;
+  let city = null;
+  let temperature = null;
+  let weatherIcon = null;
+
+  if (weatherData.dt) {
+    time = new Date(weatherData.dt * 1000).toLocaleTimeString().slice(0, 5);
+    city = weatherData.name;
+    temperature = (weatherData.main.temp - 273.15).toFixed(0);
+
+    const { id } = weatherData.weather[0];
+    if (id >= 200 && id <= 232) {
+      weatherIcon = <BsCloudLightningRain className='icon' />;
+    } else if (id >= 300 && id <= 321) {
+      weatherIcon = <BsCloudRain className='icon' />;
+    } else if (id >= 500 && id <= 531) {
+      weatherIcon = <BsCloudRain className='icon' />;
+    } else if (id >= 600 && id <= 622) {
+      weatherIcon = <BsCloudSnow className='icon' />;
+    } else if (id >= 801 && id <= 804) {
+      weatherIcon = new Date().getHours() >= 6 && new Date().getHours() <= 18 ? (
+        <BsCloudSun className='icon' />
+      ) : (
+        <BsCloudMoon className='icon' />
+      );
     }
-  
-    return (
-      <div className="app">
-        <div className="search">
-          <h1>Type you city</h1>
-          <input
-            value={location}
-            onChange={event => setLocation(event.target.value)}
-            onKeyPress={searchLocation}
-            placeholder='Enter Location'
-            type="text" />
-        </div>
-        <div className="container">
-          <div className="top">
-            <div className="location">
-              {data.main ? <h1>City: {data.name}</h1> : null }
-            </div>
-            <div className="temp">
-              {data.main ? <h1>Temperature: {data.main.temp.toFixed()}°C</h1> : null}
-            </div>
-            <div className="description">
-              {data.weather ? <h1>Sky:{data.weather[0].main}</h1> : null}
-            </div>
-          </div> 
-        </div>
-      </div>
-    );
   }
-  
-  export default App;
+
+  return (
+    <div className='app'>
+      <h1>WEATHER SITE</h1>
+      {city ? (
+        <>
+          <h2>
+            {city}, {time}
+          </h2>
+          <h3>
+            {temperature}°C
+          </h3>
+          <div className='icon'>
+            {weatherIcon}
+          </div>
+        </>
+      ) : (
+        <h4>to see the weather of your city, please allow access to your location in your browser, thank you</h4>
+      )}
+    </div>
+  );
+}
+
+export default App;
